e581152c36e39a25f79ff8354f43a7a8440bda7d
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-05-14 18:19:06 +0530

    MCQ app

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MCQ Generator",
+    version="0.0.0",
+    author="Prashant",
+    author_email="prashantbhusewar1599@gmail.com",
+    packages=find_packages()
+)
